1bdbd3b45c9c2da533aecfffa86d21bec31170d4
ado <ado@languagewire.com>
2020-10-31 15:02:04 +0200

    fixed docker repositories naming

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t sanok005/munlti-client:latest -t sanok005/munlti-client:$SHA -f ./client/Dockerfile ./client
-docker build -t sanok005/munlti-server:latest -t sanok005/munlti-server:$SHA -f ./server/Dockerfile ./server
-docker build -t sanok005/munlti-worker:latest -t sanok005/munlti-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t sanok005/multi-client:latest -t sanok005/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t sanok005/multi-server:latest -t sanok005/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t sanok005/multi-worker:latest -t sanok005/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push sanok005/multi-client:latest
 docker push sanok005/multi-server:latest
 docker push sanok005/multi-worker:latest
@@ -8,7 +8,8 @@ docker push sanok005/multi-client:$SHA
 docker push sanok005/multi-server:$SHA
 docker push sanok005/multi-worker:$SHA
 kubectl apply -f k8s
-sleep 10s
+
+echo "kubernetes deployments:"
 kubectl get deployments
 kubectl set image deployments/server-deployment server=sanok005/multi-server:$SHA
 kubectl set image deployments/client-deployment server=sanok005/multi-client:$SHA
